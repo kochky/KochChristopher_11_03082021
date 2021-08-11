3b0875ab0b8c502d8c15b81979d0acc7ebe495f1
Koch Christopher <koch.christopher@hotmail.fr>
2021-08-12 00:35:16 +0200

    fleche banner disparaisse si 1 image

--- src/composants/LocationBanner.js
+++ src/composants/LocationBanner.js
@@ -6,14 +6,14 @@ function LocationBanner(props){
     const indexImg = props.cover.indexOf(coverImg)
     const numberOfImg= props.cover.length -1
 
-  
-    return (
+     
+    return props.cover.length >1 ?(
         <div style={{background:'no-repeat url("'+(coverImg)+'")'}}  className="locationbanner">
-                <i onClick={() => {indexImg === 0  ? setCoverImg(props.cover[numberOfImg]) : setCoverImg(props.cover[indexImg -1])}} className="fas fa-chevron-left fa-5x left-arrow"></i>
+               <i onClick={() => {indexImg === 0  ? setCoverImg(props.cover[numberOfImg]) : setCoverImg(props.cover[indexImg -1])}} className="fas fa-chevron-left fa-5x left-arrow"></i>
                 <i  onClick={() => {indexImg === numberOfImg  ? setCoverImg(props.cover[0]) : setCoverImg(props.cover[indexImg +1])}} className="fas fa-chevron-right fa-5x right-arrow"></i>
         </div>
         
-    )
+    ):(<div style={{background:'no-repeat url("'+(coverImg)+'")'}}  className="locationbanner"></div>)
     
 }
 
