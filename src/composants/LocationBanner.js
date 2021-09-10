5a4b99264318179ac1418d8069ace6b1bceeeb6b
Koch Christopher <koch.christopher@hotmail.fr>
2021-09-10 15:10:38 +0200

    fix bug image

--- src/composants/LocationBanner.js
+++ src/composants/LocationBanner.js
@@ -8,7 +8,7 @@ function LocationBanner(props){
 
      
     return props.cover.length >1 ?(
-        <div style={{background:'no-repeat url("'+(coverImg)+'")'}}  className="locationbanner">
+        <div style={{background:'center no-repeat  url("'+(coverImg)+'")'}}  className="locationbanner">
                <i onClick={() => {indexImg === 0  ? setCoverImg(props.cover[numberOfImg]) : setCoverImg(props.cover[indexImg -1])}} className="fas fa-chevron-left fa-5x left-arrow"></i>
                 <i  onClick={() => {indexImg === numberOfImg  ? setCoverImg(props.cover[0]) : setCoverImg(props.cover[indexImg +1])}} className="fas fa-chevron-right fa-5x right-arrow"></i>
         </div>
