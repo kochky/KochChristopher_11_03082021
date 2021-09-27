a7956235becf6a2599328afea794cbb22278c7f1
Koch Christopher <koch.christopher@hotmail.fr>
2021-09-27 17:30:46 +0200

    readme+ hook cards

--- README.md
+++ README.md
@@ -4,6 +4,21 @@
 
 Respect maquette + developper application, et composants React à base de Class, et routes React Router
 
+### Installation et lancement 
+
+Cloner le repo  :
+
+`git clone https://github.com/kochky/KochChristopher_11_03082021`
+
+En se plaçant dans ce repo, installer ses dépendances :
+
+`npm install`
+
+Lancer le sur le port 3000 :
+
+`npm start`
+
+L'application est alors consultable à l'URL `http://localhost:3000`.
 ## For Developers
 
 ### Languages used
--- src/composants/Main.js
+++ src/composants/Main.js
@@ -3,18 +3,24 @@ import { Link } from 'react-router-dom'
 import React from "react"
 
 export default class Main extends React.Component{
+    constructor(props) {
+        super(props);
+        this.state = { data:false };
+      }
+    componentDidMount() {
+        this.setState({data: LogementList().map((logement) => (
+            <Link to={`/location/${logement.id}`} 
+            style={{backgroundImage:'url("'+(logement.cover)+'")'}} 
+                key={logement.id} id={(logement.id)} className='card'> <div>{logement.title}</div></Link>
+            ))})
+        }
 
     render(){
-        return (
-            <main className="mainhome">
-                {LogementList().map((logement) => (
-                  <Link to={`/location/${logement.id}`} 
-                  style={{
-                      backgroundImage:'url("'+(logement.cover)+'")'
-                    }} 
-                    key={logement.id} id={(logement.id)} className='card'> <div>{logement.title}</div></Link>
-                ))}
-            </main>
-        )
-    }
+        return(
+            <main className="mainhome"> 
+                {this.state.data ===false?(<div class="lds-ring"><div></div><div></div><div></div><div></div></div>)
+                :this.state.data}
+            </main> )
+        
+        }
 }
